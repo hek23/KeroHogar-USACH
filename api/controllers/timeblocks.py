c5e7c828bf6f5e6282f2c7127fb11cd907e9f1eb
Héctor Fuentealba <hector.fuentealba@usach.cl>
2019-06-11 23:49:07 -0400

    TimeBlocks fixed

--- api/controllers/timeblocks.py
+++ api/controllers/timeblocks.py
@@ -6,20 +6,26 @@ from helpers.Authenticator import requires_auth
 @current_app.route('/v1/timeblocks/available/<date>', methods=['GET'])
 @requires_auth
 def getAvailableTimeBlocks(date):
-    query = "SELECT otb.time_block_id, COUNT(*) FROM orders o INNER JOIN order_time_block otb on o.id=otb.order_id where o.delivery_date = \'{}\' AND o.delivery_status=1 GROUP BY o.delivery_date, otb.time_block_id;"
+    query = "SELECT otb.time_block_id, COUNT(*) FROM orders o INNER JOIN order_time_block otb on o.id=otb.order_id where o.delivery_date = \'{}\' AND o.delivery_status=1 GROUP BY o.delivery_date, otb.time_block_id ORDER BY o.delivery_date;"
     cursor = mysqlConnector.get_db().cursor()
     cursor.execute(query.format(date))
     ocupacion = cursor.fetchall()
     cursor.execute("SELECT id, max_orders, start, end FROM time_blocks")
     capacidad = cursor.fetchall()
     bloques= []
+    diaBase = []
+    for hora in capacidad:
+        diaBase.append(str(hora[2]) + "-" + str(hora[3]))
+
     for bloque in capacidad:
+        #Reset, dia terminado
         for ocupado in ocupacion:
             if(bloque[0] == ocupado[0]):
-                if(bloque[1] >ocupado[1]):
-                    bloques.append({
-                        "id": bloque[0],
-                        "block": bloque[2] + "-" + bloque[3]
-                    })
-    return Response(json.dumps(bloques), mimetype = 'application/json')
+                if(bloque[1] < ocupado[1]):
+                    diaBase.remove(str(bloque[2])+ "-" + str(bloque[3]))
+
+    return Response(json.dumps(diaBase), mimetype = 'application/json')
+
+
+
 
