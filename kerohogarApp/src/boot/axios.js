fb87b3e8bf0281db7f687b762277b8711a4d3cba
Nicolás Mariángel Toledo <nicolas.mariangel@usach.cl>
2019-07-08 18:37:57 -0400

    Refresh access token when expired.

--- kerohogarApp/src/boot/axios.js
+++ kerohogarApp/src/boot/axios.js
@@ -14,6 +14,40 @@ export default async ({ Vue }) => {
     return Promise.reject(err);
   });
 
+  axios.interceptors.response.use(
+    response => response,
+    error => {
+      const originalRequest = error.config;
+
+      if (error.response.status === 401 && LocalStorage.has('user')) {
+        let user = LocalStorage.getItem('user')
+        const refresh_token = user.refresh;
+
+        const axiosInstance = axios.create({
+          baseURL: 'http://165.22.120.0:5000/v1/',
+          timeout: 5000,
+        });
+        return axiosInstance.post('/users/refresh', null, {
+          headers: { Authorization: "Bearer " + refresh_token }
+        })
+          .then(({ data }) => {
+            user.token = data.token
+            LocalStorage.set('user', user);
+
+            axios.defaults.headers.Authorization = 'Bearer ' + user.token;
+            originalRequest.headers.Authorization = 'Bearer ' + user.token;
+
+            return axios(originalRequest);
+          })
+          .catch(err => {
+            console.log(err)
+          });
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
   axios.defaults.headers.post['Content-Type'] = 'application/json';
 
   Vue.prototype.$axios = axios
